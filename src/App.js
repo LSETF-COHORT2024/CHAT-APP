e9d1701ae384145fff2643d0611e6c3624b42240
OBIANUKA CHIMAOBI <obianukachimaobim@gmail.com>
2024-03-03 00:04:14 +0100

    trial

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import "./style.scss";
 import { BrowserRouter,
   Routes,
-  Route } from "react-router-dom";
+  Route, 
+  Navigate} from "react-router-dom";
+import { AuthContext } from "./context/AuthContexts";
 
 const App = () => {
+  const {currentUsers}=useContext(AuthContext)
+  const ProtectedRoute = ({children}) =>{
+    if (!currentUsers) {
+      return <Navigate to="/login"/>
+    }
+    return children
+  }
+
   return (
     <>
     <BrowserRouter>
  <Routes>
   <Route path="/">
-    <Route index element={<Home/>} />
+    <Route index element={
+        <ProtectedRoute>
+        <Home/>
+      </ProtectedRoute>
+    } />
     <Route path="register" element={<Register/>} />
     <Route path="login" element={<Login/>} />
   </Route>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 import { useNavigate,Link } from 'react-router-dom'
+import { useState } from "react";
+import {  signInWithEmailAndPassword } from "firebase/auth"; 
+import { auth } from "../firebase";
 
 const Login = () => {
-  const navigate = useNavigate()
+  const [error, setError] = useState(null);
+  const navigate =useNavigate()
 
-  function handsubmit() {  
-     navigate("/")
-  }
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const email = e.target[0].value;
+      const password = e.target[1].value;
+      
+      try {
+        await signInWithEmailAndPassword(auth, email, password)
+        navigate("/login")
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
   return (
     <div className="formContainer">
       <div class=" formWrapper">
         <span className="logo">CHAT APP</span>
         <span className="title"> Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
 
-          <button onClick={handsubmit}>Sign in</button>
+          <button >Sign in</button>
+          {error && <span>{error}</span>}
+
         </form>
         <p>You don't have an account? <Link to="/register">Register</Link></p>
       </div>
