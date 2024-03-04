84a2ad537f31e132c51c1b6a6d7da7eb93c36098
Ikoyi1st <princeik.akowe@gmail.com>
2024-03-04 12:17:41 +0100

    now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,13 @@
-// import React from "react";
-// import Profile1 from "../img/pic-person-01.jpg";
-// import { auth } from "../firebase";
-// import { signOut } from "firebase/auth";
 
-// const Navbar = () => {
-//   return (
-//     <div className="navbar">
-//       <span className="logo">Innkeeper chat</span>
-//       <div className="user">
-//         <img src={Profile1} alt="User Picture" />
-//         <span>Ceo</span>
-//         <button onClick={()=>signOut(auth)}>logout</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
-
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom"; // Import useHistory hook
-import Profile1 from "../img/pic-person-01.jpg";
+// import Profile1 from "../img/pic-person-01.jpg";
 import { auth } from "../firebase";
 import { signOut } from "firebase/auth";
+import { AuthContext } from "../context/AuthContexts";
 
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext)
   const navigate = useNavigate(); // Initialize useHistory
 
   const handleLogout = async () => {
@@ -41,8 +23,8 @@ const Navbar = () => {
     <div className="navbar">
       <span className="logo">Innkeeper chat</span>
       <div className="user">
-        <img src={Profile1} alt="User Picture" />
-        <span>Ceo</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
         <button onClick={handleLogout}>Logout</button> {/* Call handleLogout on button click */}
       </div>
     </div>
